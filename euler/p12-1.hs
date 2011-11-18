7d537bf70b76d09237f3a732546179b9b678fc6a
Simon.Wu <simon.wu@c3-e.com>
2011-11-18 19:15:37 +0800

    separated

--- euler/p12-1.hs
+++ euler/p12-1.hs
@@ -5,14 +5,12 @@ import Primeing (primeFactors)
 
 {-- |
     Project euler 12
+    Solution 1
 --}
     
 main :: IO ()
-main = print $ problem_12
+main = print $ p12 500
 
-{-- |
-    Solution 1
---}
 p12 :: Int -> Int
 p12 n = head $ filter (factorLimit n) [ smartGaus x | x <- [1..]]
 
@@ -26,8 +24,8 @@ smartGaus n
 --}
 factorLimit :: Int -> Int -> Bool
 factorLimit l n  
-  | sqrtInt n < l   = False
-  | otherwise         = length (factors  n) >= l
+  | 2 * sqrtInt n < l    = False
+  | otherwise            = length (factors  n) >= l
 
 
 factors :: Int -> [Int]
@@ -35,11 +33,3 @@ factors n = concat [ [x, n `div` x] | x <- [1..sqrtInt n], n `mod` x == 0 ]
 
 sqrtInt :: Int -> Int
 sqrtInt = truncate . sqrt . fromIntegral
-
-{-- | 
-     Solution from haskell wiki
- --}
-     
-problem_12 = head $ filter ((> 500) . nDivisors) triangleNumbers          
-  where nDivisors n = product $ map ((+1) . length) (group (primeFactors n))    
-        triangleNumbers = scanl1 (+) [1..]
