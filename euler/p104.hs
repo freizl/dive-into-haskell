21095bb88cccf8a7d98725995ddf641143541f7b
Simon.Wu <simon.wu@c3-e.com>
2011-12-13 11:59:42 +0800

    use mylib

--- euler/p104.hs
+++ euler/p104.hs
@@ -1,21 +1,15 @@
 module Main where
 
 import Data.List
+import Number.Fibonacci(fibs)
 
 main :: IO ()
-main = do
-  print $ findCertainFibIndex isFirstNinePandigit
+main = print $ snd $ head $ dropWhile (\(x,y) -> (not . isLastNinePandigit "123456789") x) (zip fibs [1..])
 
-findCertainFibIndex fn = length $ takeWhile (not . fn "123456789") fibs
+bothNinePandigit digits n = isFirstNinePandigit digits n && isLastNinePandigit digits n
 
-bothNinePandigit digits n = (isFirstNinePandigit digits n) && (isLastNinePandigit digits n)
-
-isLastNinePandigit  digits n = digits == (sort $ lastDigits 9 n)
-isFirstNinePandigit digits n = digits == (sort $ firstDigits 9 n)
-
+isLastNinePandigit  digits n = digits == sort (lastDigits 9 n)
+isFirstNinePandigit digits n = digits == sort (firstDigits 9 n)
 
 firstDigits k n = take k (show n)
 lastDigits  k n = show (n `mod` 10^k)
-
---fib n = fibs !! n
-fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
--- mylib/src/Number/Primeing.hs
+++ mylib/src/Number/Primeing.hs
@@ -1,10 +1,13 @@
 module Number.Primeing where
 
 sieve :: [Integer] -> [Integer]
+sieve []     = []
 sieve (p:xs) = p : sieve [ x | x <- xs, x `mod` p /= 0 ]
 
+primes :: [Integer]
 primes = sieve [2..]
 
+primeFactors :: Integer -> [Integer]
 primeFactors n = factor n primes
   where
     factor n (p:ps) 
