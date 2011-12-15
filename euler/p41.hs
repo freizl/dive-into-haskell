53b9c56e48276cd94f4a7623f2c484353b78b997
haisheng.wu <freizl@gmail.com>
2011-12-15 22:17:16 +0800

    a little format

--- euler/p41.hs
+++ euler/p41.hs
@@ -5,18 +5,20 @@ import Data.List
 
 main = print $ p41
 
-p41 = maximum . head $ dropWhile null (map candidates possibleP)
+p41          = maximum . head $ dropWhile null (map candidates possibleP)
 
-possibleP  = ["987654321", "87654321", "7654321", "654321"]
+possibleP    = ["987654321", "87654321", "7654321", "654321", "54321"]
 
 candidates s = filter (isPrime . read) (permutations s)
 
-isPrime n = n:[] == primeFactors n
+isPrime n    = n:[] == primeFactors n
 
 {--|
+
 "7652413"
 
 real	0m21.818s
 user	0m21.761s
 sys	0m0.016s
+
 --}
