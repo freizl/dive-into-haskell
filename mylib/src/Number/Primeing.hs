99d723680e4db513f6ce322e04742f81026786aa
haisheng.wu <freizl@gmail.com>
2011-12-15 22:15:00 +0800

    type for Primeing functions

--- mylib/src/Number/Primeing.hs
+++ mylib/src/Number/Primeing.hs
@@ -3,14 +3,14 @@ module Number.Primeing
         primeFactors)
        where
 
-sieve :: [Integer] -> [Integer]
+sieve :: (Integral a) => [a] -> [a] --[Integer] -> [Integer]
 sieve []     = []
 sieve (p:xs) = p : sieve [ x | x <- xs, x `mod` p /= 0 ]
 
-primes :: [Integer]
+primes :: (Integral a) => [a]
 primes = sieve [2..]
 
-primeFactors :: Integer -> [Integer]
+primeFactors :: (Integral a) => a -> [a]
 primeFactors n = factor n primes
   where
     factor n (p:ps) 
