58f2d8f33d1afb3dad9ad8df63cfa48edbf7eeb3
Simon.Wu <simon.wu@c3-e.com>
2011-12-22 17:10:55 +0800

    sandbox codes

---
+++ web/yesod/misc/hello.hs
@@ -0,0 +1,19 @@
+-- START
+{-# LANGUAGE TypeFamilies, QuasiQuotes, MultiParamTypeClasses,
+TemplateHaskell, OverloadedStrings #-}
+import Yesod
+data Links = Links
+mkYesod "Links" [parseRoutes|
+/ HomeR GET
+/page1 Page1R GET
+/page2 Page2R GET
+|]
+
+instance Yesod Links where
+  approot _ = ""
+  
+getHomeR = defaultLayout $ do addHamlet [hamlet|<a href="@{Page1R}">Go to page 1!|]
+getPage1R = defaultLayout $ do addHamlet [hamlet|<a href="@{Page2R}">Go to page 2!|]
+getPage2R = defaultLayout $ do addHamlet [hamlet|<a href="@{HomeR}">Go home!|]
+main = warpDebug 3000 Links
+-- STOP
