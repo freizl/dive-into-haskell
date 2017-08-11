edd4f766c9c284f624022f9c7add31580f98b7be
Haisheng.Wu <haisheng.wu@okta.com>
2017-08-11 15:11:29 -0700

    one more test case.

--- readings/pearls-of-FP/test/MinFreeNumberSpec.hs
+++ readings/pearls-of-FP/test/MinFreeNumberSpec.hs
@@ -6,37 +6,39 @@ import Test.QuickCheck
 
 import MinFreeNumber
 
-sample1, sample2 :: [Integer]
-sample0 = []
-sample1 = [08,23,09,0,12,11,10,13,07,41,04,14,21,05,17,03,19,02,06]
-sample2 = [102..1000] ++ [0..100]
-sample3 = [0]
-sample4 = [1]
-sample5 = [10]
-sample6 = [10..15] ++ [0..8]
+sampleData1, sampleData2 :: [Integer]
+sampleData0 = []
+sampleData1 = [08,23,09,0,12,11,10,13,07,41,04,14,21,05,17,03,19,02,06]
+sampleData2 = [102..1000] ++ [0..100]
+sampleData3 = [0]
+sampleData4 = [1]
+sampleData5 = [10]
+sampleData6 = [10..15] ++ [0..8]
+sampleData7 = [0..8] ++ [10..18]
 
 test :: SpecWith ()
 test =
   describe "MinFreeNumber" $ do
 
-    mapM_ runTest [ (sample0, 0)
-                  , (sample1, 1)
-                  , (sample2, 101)
-                  , (sample3, 1)
-                  , (sample4, 0)
-                  , (sample5, 0)
-                  , (sample6, 9)
+    mapM_ runTest [ (sampleData0, 0)
+                  , (sampleData1, 1)
+                  , (sampleData2, 101)
+                  , (sampleData3, 1)
+                  , (sampleData4, 0)
+                  , (sampleData5, 0)
+                  , (sampleData6, 9)
+                  , (sampleData7, 9)
                   ]
     --xit "returns identical result from minfree and minfree2" $ property $
       -- \xs -> minfree xs `shouldBe` minfree2 xs
 
 runTest :: ([Integer], Integer) -> SpecWith (Arg Expectation)
-runTest (sample, expected) =
-  it ("returns " ++ show expected ++ " for input " ++ showSample sample) $ do
-    minfree sample `shouldBe` expected
-    minfree sample `shouldBe` minfree2 sample
+runTest (sampleData, expected) =
+  it ("returns " ++ show expected ++ " for input " ++ showSampleData sampleData) $ do
+    minfree sampleData `shouldBe` expected
+    minfree sampleData `shouldBe` minfree2 sampleData
 
-showSample :: [Integer] -> String
-showSample sample
- | length sample <= 5 = show sample
- | otherwise = show (take 5 sample) ++ "..."
+showSampleData :: [Integer] -> String
+showSampleData sampleData
+ | length sampleData <= 5 = show sampleData
+ | otherwise = show (take 5 sampleData) ++ "..."
