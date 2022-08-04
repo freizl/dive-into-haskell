f75e457884ec62f58cba984b5755cc83dabff024
Haisheng Wu (M) <freizl@gmail.com>
2022-08-04 16:00:38 -0700

    try part 3 😕

--- readings/haskell-book/26/morra/Main.hs
+++ readings/haskell-book/26/morra/Main.hs
@@ -1,18 +1,24 @@
 module Main where
 
+import Control.Monad
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.State
 import System.Environment
 import System.IO
 import System.Random
-import Text.Read
+import Text.Read (readEither)
 
 type Score = Int
 
 type PlayName = String
 
-type Morra a = StateT [(PlayName, Score)] IO a
+data AppState = AppState
+  { scores :: [(PlayName, Score)],
+    humanBehavior :: [Int]
+  }
+
+type Morra a = StateT AppState IO a
 
 desiredScore :: (Int, Int)
 desiredScore = (1, 2)
@@ -52,9 +58,15 @@ humanGuess = do
 
 gameAiToHuman :: Morra ()
 gameAiToHuman = do
-  a <- liftIO aiGuess
+  s1 <- get
+  let hs = humanBehavior s1
+  a <- if length hs == 3
+    then liftIO aiGuess -- FIXME: I'm not fully understand the 3-gram
+    else liftIO aiGuess
   b <- liftIO humanGuess
-  modify (\s -> ("C", a) : ("P", b) : s)
+  let s2 = if length hs < 3 then s1 {humanBehavior = hs ++ [b]} else s1
+  let s3 = s2 {scores = [("C", a), ("P", b)]}
+  put s3
 
 gameHumanToHuman :: Morra ()
 gameHumanToHuman = do
@@ -64,16 +76,23 @@ gameHumanToHuman = do
     y <- humanGuess
     clearScreen
     return (x, y)
-  modify (\s -> ("P1", a) : ("P2", b) : s)
+  modify (\s -> s {scores = [("P1", a), ("P2", b)]})
 
-main :: IO ()
-main = do
-  args <- getArgs
-  putStrLn "=== Morra - Have Fun ==="
-  let game = if null args then gameAiToHuman else gameHumanToHuman
-  (_, [(p1, a), (p2, b)]) <- runStateT game []
+runGame :: AppState -> Morra () -> IO AppState
+runGame appState game = do
+  (_, s) <- runStateT game appState
+  let [(p1, a), (p2, b)] = scores s
   putStrLn (p1 ++ ": " ++ show a)
   putStrLn (p2 ++ ": " ++ show b)
   if even (a + b)
     then putStrLn (p1 ++ " - wins")
     else putStrLn (p2 ++ " - Player wins")
+  runGame s game
+
+main :: IO ()
+main = do
+  args <- getArgs
+  putStrLn "=== Morra - Have Fun ==="
+  let defaultAppState = AppState {scores = [], humanBehavior = []}
+  let game = if null args then gameAiToHuman else gameHumanToHuman
+  void $ runGame defaultAppState game
