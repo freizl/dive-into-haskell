141899437628f41c4980f879a7f6367c3a589efc
Haisheng Wu (M) <freizl@gmail.com>
2022-08-04 15:29:32 -0700

    chapter 26 morra - part2

--- readings/haskell-book/26/morra/Main.hs
+++ readings/haskell-book/26/morra/Main.hs
@@ -1,20 +1,24 @@
 module Main where
 
-import System.IO
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.State
-import System.Environment (getArgs)
-import Text.Read
+import System.Environment
+import System.IO
 import System.Random
+import Text.Read
 
 type Score = Int
-type Morra a = StateT [ Score ] IO a
 
+type PlayName = String
+
+type Morra a = StateT [(PlayName, Score)] IO a
+
+desiredScore :: (Int, Int)
+desiredScore = (1, 2)
 
 aiGuess :: IO Int
-aiGuess = randomRIO (1, 2)
-
+aiGuess = randomRIO desiredScore
 
 getLineWithoutEcho = do
   hSetEcho stdin False
@@ -23,30 +27,53 @@ getLineWithoutEcho = do
   putChar '\n'
   return str
 
+-- https://stackoverflow.com/a/9359947
+-- maybe try https://hackage.haskell.org/package/terminfo
+--
+clearScreen :: IO ()
+clearScreen = putStr "\ESC[1J"
 
 humanGuess :: IO Int
 humanGuess = do
   hSetBuffering stdout NoBuffering
   putStr "Guess your number: "
   str <- getLineWithoutEcho
-  -- TODO: shall we constrain to number 1 or 2.?
   case readEither str of
-    Right i -> return i
-    Left e -> putStrLn e >> putStrLn "Not a number, please try again" >> humanGuess
+    Right i ->
+      if fst desiredScore == i || snd desiredScore == i
+        then return i
+        else
+          putStrLn "Not desired number (1,2). Try again"
+            >> humanGuess
+    Left e ->
+      putStr e
+        >> putStrLn ". Not a number, please try again"
+        >> humanGuess
 
-game :: Morra ()
-game = do
+gameAiToHuman :: Morra ()
+gameAiToHuman = do
   a <- liftIO aiGuess
   b <- liftIO humanGuess
-  modify (\s -> a:b:s)
+  modify (\s -> ("C", a) : ("P", b) : s)
+
+gameHumanToHuman :: Morra ()
+gameHumanToHuman = do
+  (a, b) <- liftIO $ do
+    x <- humanGuess
+    clearScreen
+    y <- humanGuess
+    clearScreen
+    return (x, y)
+  modify (\s -> ("P1", a) : ("P2", b) : s)
 
 main :: IO ()
 main = do
+  args <- getArgs
   putStrLn "=== Morra - Have Fun ==="
-  (_, [a,b]) <- runStateT game []
-  putStrLn ("C: " ++ show a)
-  putStrLn ("P: " ++ show b)
-  if even (a + b) then
-    putStrLn "- Computer wins"
-  else
-    putStrLn "- Player wins"
+  let game = if null args then gameAiToHuman else gameHumanToHuman
+  (_, [(p1, a), (p2, b)]) <- runStateT game []
+  putStrLn (p1 ++ ": " ++ show a)
+  putStrLn (p2 ++ ": " ++ show b)
+  if even (a + b)
+    then putStrLn (p1 ++ " - wins")
+    else putStrLn (p2 ++ " - Player wins")
